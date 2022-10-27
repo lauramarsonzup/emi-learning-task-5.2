dfc0e60b83a644f2495d71e2bab5b239d58bfa6a
Laura Marson <laura.marson@zup.com.br>
2022-10-27 10:45:45 -0300

    finished task

--- LearningTask-5.2/Models/RelatorioDeDespesas.swift
+++ LearningTask-5.2/Models/RelatorioDeDespesas.swift
@@ -1,20 +1,52 @@
-////
-////  RelatorioDeDespesas.swift
-////  LearningTask-5.2
-////
-////  Created by rafael.rollo on 15/03/2022.
-////
 //
-//import Foundation
+//  RelatorioDeDespesas.swift
+//  LearningTask-5.2
 //
-//struct Despesas {
-//    private(set) var itens: [Despesa] = []
+//  Created by rafael.rollo on 15/03/2022.
 //
-//    var total: Decimal {
-//        return itens.map({ $0.valor }).reduce(0, +)
-//    }
-//
-//    mutating func adiciona(_ item: Despesa) {
-//        itens.append(item)
-//    }
-//}
+
+import Foundation
+
+struct RelatorioDeDespesas {
+    let dataDeCriacao: Date = Date.now
+    private(set) var despesas: Despesas = Despesas()
+    private(set) var valorTotal: Decimal = 0
+    
+    mutating func adiciona(_ despesa: Despesa) {
+        despesas.adiciona(despesa)
+        valorTotal = despesas.total
+    }
+}
+
+struct Despesas {
+    private(set) var itens: [Despesa] = []
+
+    var total: Decimal {
+        return itens.map({ $0.valor }).reduce(0, +)
+    }
+
+    mutating func adiciona(_ item: Despesa) {
+        itens.append(item)
+    }
+}
+
+struct Despesa {
+    let titulo: String
+    let tipo: Tipo
+    let valor: Decimal
+    
+    enum Tipo: Int {
+        case eletronicos = 1
+        case escritorio = 2
+        
+        var text: String {
+            switch self {
+            case .eletronicos:
+                return "Eletrônicos"
+                    
+            case .escritorio:
+                return "Escritório"
+            }
+        }
+    }
+}
